72a007fe7f6c3bc57ead4627b93008599df35470
dawnfire05 <dawnfire05@naver.com>
2024-10-07 17:01:27 +0900

    fix: db name

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Helper function to connect to the SQLite database
 def get_db_connection():
-    conn = sqlite3.connect('db_univ_7.db')
+    conn = sqlite3.connect('db_univ7.db')
     conn.row_factory = sqlite3.Row
     return conn
 
